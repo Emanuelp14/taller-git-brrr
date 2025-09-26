f20248f95d1a822a4c97bd7d79210d9fb2d1e092
Emanuelp14 <emanuelpulgarin0045@gmail.com>
2025-09-25 21:01:02 -0500

    Agrego funciones suma y resta con bloque principal

---
+++ main.py
@@ -0,0 +1,14 @@
+# Equipo brrrr
+# Descripción: Proyecto de prueba para practicar Git y GitHub.
+# Contiene funciones básicas de operaciones matemáticas simples.
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Probando funciones matemáticas:")
+    print("5 + 3 =", suma(5, 3))
+    print("10 - 4 =", resta(10, 4))
