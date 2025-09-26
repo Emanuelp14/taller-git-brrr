60c57e5616b7ed4d24ac07d696f19e8dc23750b1
Emanuelp14 <emanuelpulgarin0045@gmail.com>
2025-09-26 15:35:22 -0500

    Agregada función modulo por Emanuel Pulgarin

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ def dividir(a, b):
     else:
         return "Error: división entre cero"
 
+# Función módulo
+def modulo(a, b):
+    if b != 0:
+        return a % b
+    else:
+        return "Error: división entre cero"
+
 
 # Bloque principal
 if __name__ == "__main__":
@@ -33,3 +40,4 @@ if __name__ == "__main__":
     print("5 * 3 =", multiplicar(5, 3))
     print("2 ** 3 =", potencia(2, 3))
     print("División 20 / 5 =", dividir(20, 5))
+    print("10 % 3 =", modulo(10, 3))
